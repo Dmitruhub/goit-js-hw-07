78634c8dd3d4e7aa761e25dc5840f9d98b068dd5
Dmitruhub <dmitrumail@gmail.com>
2022-09-26 21:46:02 +0300

    task 7 rewiev

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,32 +2,49 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
-const photos = document.querySelector('.gallery');
+console.log(createPhotosMarcup(galleryItems));
 
-function createPhotoCardsMarcap(galleryItems) {
-  const marcup = galleryItems.map((galleryItem) => {
-    return `<div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </div>`;
-  });
-  console.log(marcup);
+const galleryContainer = document.querySelector('.gallery');
+const photosMarcup = createPhotosMarcup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', photosMarcup);
+
+function createPhotosMarcup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
 }
+galleryContainer.addEventListener('click', openModal);
+function openModal(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
 
-// const input = document.querySelector('#validation-input');
-// const limit = +input.dataset.length;
-// input.classList.add('invalid');
-// const check = () => {
-//   if (input.value.length === limit) {
-//     input.classList.replace('invalid', 'valid');
-//   } else {
-//     input.classList.replace('valid', 'invalid');
-//   }
-// };
-// input.addEventListener('blur', check);
+  const options = {
+    onShow: () => window.addEventListener('keydown', onEscClick),
+    onclose: () => window.removeEventListener('keydown', onEscClick),
+  };
+
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}">`,
+    options
+  );
+
+  instance.show();
+  function onEscClick(evt) {
+    if (evt.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+console.log(createPhotosMarcup(galleryItems));
+
+const galleryContainer = document.querySelector('.gallery');
+const photosMarcup = createPhotosMarcup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', photosMarcup);
+
+function createPhotosMarcup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href=${original}><img class="gallery__image" src="${preview}" alt="${description}"/></a>`;
+    })
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
